30d66f2ff5b4619d9b8b2a0c8376d626e7dbf426
Your santhoshlesk <santhoshlesk@gmail.com>
2022-11-03 19:57:28 +0530

    add initial files

---
+++ index.js
@@ -0,0 +1,26 @@
+function menu(){
+	var a=document.getElementById("nav-div");
+	var b=document.getElementById("nav-svg");
+	var c=document.getElementById("nav-svg1");
+	b.style.display="none";
+	c.style.display="block";
+	if(a.style.display=="block"){
+		a.style.display="none";
+	}
+	else{
+		a.style.display="block";
+	}
+}
+function menu1(){
+	var a=document.getElementById("nav-div");
+	var b=document.getElementById("nav-svg");
+	var c=document.getElementById("nav-svg1");
+	c.style.display="none";
+	b.style.display="block";
+	if(a.style.display=="block"){
+		a.style.display="none";
+	}
+	else{
+		a.style.display="block";
+	}
+}
